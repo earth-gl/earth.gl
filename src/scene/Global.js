c367553341e78edf4b595d34ade11de08c40f4b6
axmand <815966508@qq.com>
2019-01-03 23:52:35 +0800

    Add reference

--- src/scene/Global.js
+++ src/scene/Global.js
@@ -1,5 +1,7 @@
 /**
  * 绘制全球
+ * https://github.com/AnalyticalGraphicsInc/cesium/blob/master/Source/Scene/Globe.js
+ * https://github.com/AnalyticalGraphicsInc/cesium/blob/22dce1d9aaf480b0cbea6148b05a4c482ce80f00/Source/Core/Ellipsoid.js
  */
 class Global{
 
