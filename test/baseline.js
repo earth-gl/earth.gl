b01c158c8677d3ead3993a2243210d43e6b917c4
axmand <815966508@qq.com>
2019-01-03 14:02:02 +0800

    FIX

--- test/baseline.js
+++ test/baseline.js
@@ -1,12 +1,12 @@
 /**
  * build document environment
  */
-
 const webgl = require("webgl-raub");
+const { Document } = require("glfw-raub");
 Document.setWebgl(webgl);
+const doc = new Document();
+global.document = global.window = doc;
 
 /**
  * 
  */
-
- const doc = new Document();
