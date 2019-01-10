573916692f84b0050de9d06daecfe191dbc21bc3
axmand <815966508@qq.com>
2019-01-10 12:53:48 +0800

    fx

---
+++ src/core/TileSchema.js
@@ -0,0 +1,4 @@
+/**
+ * https://github.com/AnalyticalGraphicsInc/cesium/blob/22dce1d9aaf480b0cbea6148b05a4c482ce80f00/Source/Core/GeographicTilingScheme.js
+ * https://github.com/AnalyticalGraphicsInc/cesium/blob/22dce1d9aaf480b0cbea6148b05a4c482ce80f00/Source/Core/WebMercatorTilingScheme.js
+ */
