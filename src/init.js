ec6a1f63298a67a860a052447f18f1e6dc9ffc9f
axmand <815966508@qq.com>
2019-11-27 17:10:37 +0800

    revise

--- src/init.js
+++ src/init.js
@@ -1,9 +1,5 @@
-const GGlobal = require('./scene/GGlobal'),
-    GSurface = require('./scene/GSurface'),
-    G3DTiles = require('./scene/G3DTiles'),
-    //loader
-    GLTFLoader = require('./loader/GLTFLoader'),
-    B3DMLoader = require('./loader/B3DMLoader'),
+const Global = require('./Global'),
+    // GSurface = require('./scene/Surface'),
     //camera
     PerspectiveCamera = require('./camera/PerspectiveCamera');
 
@@ -12,14 +8,9 @@ module.exports = {
         camera: {
             PerspectiveCamera: PerspectiveCamera
         },
-        Loader: {
-            GLTFLoader: GLTFLoader,
-            B3DMLoader: B3DMLoader
-        },
+        Global: Global,
         scene: {
-            GGlobal: GGlobal,
-            GSurface: GSurface,
-            G3DTiles: G3DTiles
+            // GSurface: GSurface,
         },
     }
 };
