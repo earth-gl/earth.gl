32f9be5d0a1000dc0daa858a255a378c43aec2d8
axmand <815966508@qq.com>
2020-10-07 00:34:57 +0800

    fixed

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,43 @@
 export class App {
 
-    public canvas: HTMLCanvasElement;
+    private canvas: HTMLCanvasElement;
 
-    public context: GPUCanvasContext;
+    private context: GPUCanvasContext;
+
+    private device: GPUDevice;
+
+    private adapter: GPUAdapter;
+
+    private format: GPUTextureFormat = 'bgra8unorm';
 
     constructor(rootElement: HTMLElement) {
         const w = rootElement.clientWidth;
         const h = rootElement.clientHeight;
-        this.canvas =document.createElement('canvas');
+        this.canvas = document.createElement('canvas');
         this.canvas.width = w;
         this.canvas.height = h;
         this.canvas.style.width = '100%';
         this.canvas.style.height = '100%';
         rootElement.appendChild(this.canvas);
-        this.context = <unknown>this.canvas.getContext('gpupresent') as GPUCanvasContext;
     }
+
+    /**
+     * 初始化adapter
+     */
+    private async InitWebGPU() {
+
+        this.adapter = await navigator.gpu?.requestAdapter({
+            powerPreference: 'high-performance'
+        });
+
+        this.device = await this.adapter.requestDevice();
+        //
+        this.context = <unknown>this.canvas.getContext('gpupresent') as GPUCanvasContext;
+
+        this.format = await this.context.getSwapChainPreferredFormat(this.device);
+    }
+
+
+
 
 }
