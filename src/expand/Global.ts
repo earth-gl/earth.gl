728ca2b3ab0d1728e8c38749e9c567eaa7c717c6
axmand <815966508@qq.com>
2020-11-01 13:08:25 +0800

    确定扩展写法

--- src/expand/Global.ts
+++ src/expand/Global.ts
@@ -19,11 +19,28 @@ class Global extends EventEmitter {
      * 
      */
     private _devicePixelRatio: number;
+    /**
+     * Global启动时执行的钩子
+     */
+    public static LoadHooks: Array<{ func: Function, args: any[] }> = new Array<{ func: Function, args: any[] }>();
 
     constructor(options: option) {
         super();
         this._canvas = (isString(options.canvas) ? document.getElementById(options.canvas as string) : options.canvas) as HTMLCanvasElement;
         this._devicePixelRatio = options.devicePixelRatio | window.devicePixelRatio;
+
+    }
+
+    private _load():void{
+        //hook
+        Global.LoadHooks.forEach((hook) => {
+            const { func, args } = hook;
+            func.apply(this, args);
+        })
+    }
+
+    static RegistHook(func: Function, ...args: any[]) {
+        Global.LoadHooks.push({ func, args });
     }
 }
 
--- src/expand/handler/Global.DOMEvents.ts
+++ src/expand/handler/Global.DOMEvents.ts
@@ -1,9 +1,20 @@
 import { Global } from './../Global';
-
+/**
+ * 合并申明
+ * reference https://www.tslang.cn/docs/handbook/declaration-merging.html
+ */
 declare module './../Global' {
     interface Global {
-        name:string;
+        name: string;
+        pluginFunc():void;
     }
 }
 
 Global.prototype.name = "ssss";
+
+Global.prototype.pluginFunc = function(){
+    const ctx = (this as Global);
+    ctx.name = "ssss1";
+}
+
+Global.RegistHook(Global.prototype.pluginFunc);
