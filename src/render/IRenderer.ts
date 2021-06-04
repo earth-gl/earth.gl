937f0169441aa77580bcbf0b13813bc6757c9c32
huangkui <815966508@qq.com>
2021-06-04 16:46:22 +0800

    Update IRenderer.ts

--- src/render/IRenderer.ts
+++ src/render/IRenderer.ts
@@ -1,5 +1,14 @@
-interface IRenderer {
+import { Camera } from "../camera/Camera";
 
+/**
+ * - Globe
+ * - Sketchpad
+ */
+interface IRenderer {
+    /** 
+     * 执行一次渲染
+     */
+    render(framestamp: number, camera: Camera): void;
 }
 
 export { IRenderer }
